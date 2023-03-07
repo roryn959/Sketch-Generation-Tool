7451d342343066448d590fd67ab3c25f1004fcc9
roryn959 <zcabrbn@ucl.ac.uk>
2023-03-07 16:26:19 +0000

    repo setup

---
+++ remake/main.py
@@ -0,0 +1,140 @@
+import numpy as np
+import tensorflow as tf
+import svgwrite
+import utils, model, rnn, sketch_rnn_train, magenta_training, os, json
+
+# libraries required for visualisation:
+from IPython.display import SVG, display
+import PIL
+from PIL import Image
+import matplotlib.pyplot as plt
+
+# set numpy output to something sensible
+np.set_printoptions(precision=8, edgeitems=6, linewidth=200, suppress=True)
+
+# little function that displays vector images and saves them to .svg
+def draw_strokes(data, factor=0.2, svg_filename = 'picture'):
+  tf.compat.v1.gfile.MakeDirs(os.path.dirname(svg_filename))
+  min_x, max_x, min_y, max_y = utils.get_bounds(data, factor)
+  dims = (50 + max_x - min_x, 50 + max_y - min_y)
+  dwg = svgwrite.Drawing(svg_filename, size=dims)
+  dwg.add(dwg.rect(insert=(0, 0), size=dims,fill='white'))
+  lift_pen = 1
+  abs_x = 25 - min_x 
+  abs_y = 25 - min_y
+  p = "M%s,%s " % (abs_x, abs_y)
+  command = "m"
+  for i in range(len(data)):
+    if (lift_pen == 1):
+      command = "m"
+    elif (command != "l"):
+      command = "l"
+    else:
+      command = ""
+    x = float(data[i,0])/factor
+    y = float(data[i,1])/factor
+    lift_pen = data[i, 2]
+    p += command+str(x)+","+str(y)+" "
+  the_color = "black"
+  stroke_width = 1
+  dwg.add(dwg.path(p).stroke(the_color,stroke_width).fill("none"))
+  dwg.save()
+  display(SVG(dwg.tostring()))
+
+# generate a 2D grid of many vector drawings
+def make_grid_svg(s_list, grid_space=10.0, grid_space_x=16.0):
+  def get_start_and_end(x):
+    x = np.array(x)
+    x = x[:, 0:2]
+    x_start = x[0]
+    x_end = x.sum(axis=0)
+    x = x.cumsum(axis=0)
+    x_max = x.max(axis=0)
+    x_min = x.min(axis=0)
+    center_loc = (x_max+x_min)*0.5
+    return x_start-center_loc, x_end
+  x_pos = 0.0
+  y_pos = 0.0
+  result = [[x_pos, y_pos, 1]]
+  for sample in s_list:
+    s = sample[0]
+    grid_loc = sample[1]
+    grid_y = grid_loc[0]*grid_space+grid_space*0.5
+    grid_x = grid_loc[1]*grid_space_x+grid_space_x*0.5
+    start_loc, delta_pos = get_start_and_end(s)
+
+    loc_x = start_loc[0]
+    loc_y = start_loc[1]
+    new_x_pos = grid_x+loc_x
+    new_y_pos = grid_y+loc_y
+    result.append([new_x_pos-x_pos, new_y_pos-y_pos, 0])
+
+    result += s.tolist()
+    result[-1][2] = 1
+    x_pos = new_x_pos+delta_pos[0]
+    y_pos = new_y_pos+delta_pos[1]
+  return np.array(result)
+
+def load_env_compatible(data_dir, model_dir):
+  """Loads environment for inference mode, used in jupyter notebook."""
+  # modified https://github.com/tensorflow/magenta/blob/master/magenta/models/sketch_rnn/sketch_rnn_train.py
+  # to work with depreciated tf.HParams functionality
+  model_params = model.get_default_hparams()
+  with tf.compat.v1.gfile.Open(os.path.join(model_dir, 'model_config.json'), 'r') as f:
+    data = json.load(f)
+  fix_list = ['conditional', 'is_training', 'use_input_dropout', 'use_output_dropout', 'use_recurrent_dropout']
+  for fix in fix_list:
+    data[fix] = (data[fix] == 1)
+  model_params.parse_json(json.dumps(data))
+  return sketch_rnn_train.load_dataset(data_dir, model_params, inference_mode=True)
+
+def encode(input_strokes):
+  strokes = utils.to_big_strokes(input_strokes).tolist()
+  strokes.insert(0, [0, 0, 1, 0, 0])
+  seq_len = [len(input_strokes)]
+  draw_strokes(utils.to_normal_strokes(np.array(strokes)))
+  return sess.run(eval_model.batch_z, feed_dict={eval_model.input_data: [strokes], eval_model.sequence_lengths: seq_len})[0]
+
+def load_model_compatible(model_dir):
+  """Loads model for inference mode, used in jupyter notebook."""
+  model_params = model.get_default_hparams()
+  with tf.compat.v1.gfile.Open(os.path.join(model_dir, 'model_config.json'), 'r') as f:
+    data = json.load(f)
+  fix_list = ['conditional', 'is_training', 'use_input_dropout', 'use_output_dropout', 'use_recurrent_dropout']
+  for fix in fix_list:
+    data[fix] = (data[fix] == 1)
+  model_params.parse_json(json.dumps(data))
+
+  model_params.batch_size = 1  # only sample one at a time
+  eval_model_params = model.copy_hparams(model_params)
+  eval_model_params.use_input_dropout = 0
+  eval_model_params.use_recurrent_dropout = 0
+  eval_model_params.use_output_dropout = 0
+  eval_model_params.is_training = 0
+  sample_model_params = model.copy_hparams(eval_model_params)
+  sample_model_params.max_seq_len = 1  # sample one point at a time
+  return [model_params, eval_model_params, sample_model_params]
+
+def decode(z_input=None, draw_mode=True, temperature=0.1, factor=0.2):
+  z = None
+  if z_input is not None:
+    z = [z_input]
+  sample_strokes, m = model.sample(sess, sample_model, seq_len=eval_model.hps.max_seq_len, temperature=temperature, z=z)
+  strokes = utils.to_normal_strokes(sample_strokes)
+  if draw_mode:
+    draw_strokes(strokes, factor)
+  return strokes
+
+if __name__ == "__main__":
+    tf.compat.v1.disable_v2_behavior()
+    data_dir = 'http://github.com/hardmaru/sketch-rnn-datasets/raw/master/aaron_sheep/'
+    model_dir = '/tmp/sketch_rnn/models/aaron_sheep/lstm'
+    [train_set, valid_set, test_set, hps_model, eval_hps_model, sample_hps_model] = load_env_compatible(data_dir, model_dir)
+    myModel = model.Model(hps_model)
+    eval_model = model.Model(eval_hps_model, reuse=True)
+    sample_model = model.Model(sample_hps_model, reuse=True)
+    sess = tf.compat.v1.InteractiveSession()
+    sess.run(tf.compat.v1.global_variables_initializer())
+    sketch_rnn_train.load_checkpoint(sess, model_dir)
+    strokes = test_set.random_sample()
+    draw_strokes(strokes)
